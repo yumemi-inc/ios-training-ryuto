6c545bb544b0033cba49f8a2674f41ca8d475720
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-06 15:24:07 +0900

    add: PrefectureListView.swiftを追加

---
+++ iOS-Training-Ryuto/PrefectureListView.swift
@@ -0,0 +1,20 @@
+//
+//  PrefectureListView.swift
+//  iOS-Training-Ryuto
+//
+//  Created by 岩本 竜斗 on 2023/06/06.
+//
+
+import SwiftUI
+
+struct PrefectureListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PrefectureListView_Previews: PreviewProvider {
+    static var previews: some View {
+        PrefectureListView()
+    }
+}
