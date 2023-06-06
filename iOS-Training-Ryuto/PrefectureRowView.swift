e8ec9dde516fabe6d66dcc7fe71c026301106b58
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-06 15:38:41 +0900

    add: PrefectureRowView.swiftを追加

---
+++ iOS-Training-Ryuto/PrefectureRowView.swift
@@ -0,0 +1,20 @@
+//
+//  PrefectureRowView.swift
+//  iOS-Training-Ryuto
+//
+//  Created by 岩本 竜斗 on 2023/06/06.
+//
+
+import SwiftUI
+
+struct PrefectureRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PrefectureRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        PrefectureRowView()
+    }
+}
