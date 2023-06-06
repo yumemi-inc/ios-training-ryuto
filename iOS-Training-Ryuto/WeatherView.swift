2478404f91a249a280c00359b851703fc3641bc0
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-06 18:59:53 +0900

    feat: 天気画面の背景色を変更

--- iOS-Training-Ryuto/WeatherView.swift
+++ iOS-Training-Ryuto/WeatherView.swift
@@ -9,7 +9,12 @@ import SwiftUI
 
 struct WeatherView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .cyan]),
+                           startPoint: .top,
+                           endPoint: .bottom)
+            .ignoresSafeArea()
+        }
     }
 }
 
