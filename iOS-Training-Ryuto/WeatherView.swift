51634c488e6b613b558dd5d28681758a8034ad58
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-06 18:54:05 +0900

    add: WeatherView.swiftを追加

---
+++ iOS-Training-Ryuto/WeatherView.swift
@@ -0,0 +1,20 @@
+//
+//  WeatherView.swift
+//  iOS-Training-Ryuto
+//
+//  Created by 岩本 竜斗 on 2023/06/06.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct WeatherView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherView()
+    }
+}
