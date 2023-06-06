ed5ad4d21c1d4a794c5475d4297583df8a72805f
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-06 15:40:22 +0900

    feat: PrefectureRowViewのUIを実装

--- iOS-Training-Ryuto/PrefectureRowView.swift
+++ iOS-Training-Ryuto/PrefectureRowView.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct PrefectureRowView: View {
+    let prefecture: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 10) {
+            Image(prefecture)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50)
+            Text(prefecture)
+        }
     }
 }
 
 struct PrefectureRowView_Previews: PreviewProvider {
     static var previews: some View {
-        PrefectureRowView()
+        PrefectureRowView(prefecture: "東京都")
     }
 }
