ddb7a655e0a3ff06981cb6f955f3c142fc90cda7
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-06 15:08:50 +0900

    Initial Commit

---
+++ iOS-Training-Ryuto/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  iOS-Training-Ryuto
+//
+//  Created by 岩本 竜斗 on 2023/06/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iOS-Training-Ryuto/iOS_Training_RyutoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_Training_RyutoApp.swift
+//  iOS-Training-Ryuto
+//
+//  Created by 岩本 竜斗 on 2023/06/06.
+//
+
+import SwiftUI
+
+@main
+struct iOS_Training_RyutoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
