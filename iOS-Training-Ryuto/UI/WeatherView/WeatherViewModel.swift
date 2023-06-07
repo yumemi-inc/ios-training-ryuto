aba742279d8244dec427417d6b141a4e98496abe
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-07 18:12:11 +0900

    feat: yumemiWeatherAPIClientプロパティを追加

--- iOS-Training-Ryuto/UI/WeatherView/WeatherViewModel.swift
+++ iOS-Training-Ryuto/UI/WeatherView/WeatherViewModel.swift
@@ -6,7 +6,14 @@
 //
 
 import Foundation
+import Combine
 
 final class WeatherViewModel: ObservableObject {
     @Published private(set) var weather: Weather? = nil
+    
+    private let yumemiWeatherAPIClient: YumemiWeatherAPIClientProtocol
+    
+    init(yumemiWeatherAPIClient: YumemiWeatherAPIClientProtocol = YumemiWeatherAPIClient()) {
+        self.yumemiWeatherAPIClient = yumemiWeatherAPIClient
+    }
 }
