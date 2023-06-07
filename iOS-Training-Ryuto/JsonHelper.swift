a5d346ad5bb970ff790cae5a6a2f06378d0883b2
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-07 18:27:08 +0900

    feat: createJSON関数を追加

--- iOS-Training-Ryuto/JsonHelper.swift
+++ iOS-Training-Ryuto/JsonHelper.swift
@@ -8,5 +8,13 @@
 import Foundation
 
 final class JsonHelper {
-    
+    static func createJSON(_ object: [String: String]) throws -> String {
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: object)
+            guard let jsonString = String(bytes: jsonData, encoding: .utf8) else { return "" }
+            return jsonString
+        } catch {
+            throw error
+        }   
+    }
 }
