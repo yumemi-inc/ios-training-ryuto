d8e7543d9ac70d29d558a9b788ce5f33375ed1d7
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-07 18:03:08 +0900

    add: YumemiWeatherAPIClient.swiftを追加

---
+++ iOS-Training-Ryuto/YumemiWeatherAPIClient.swift
@@ -0,0 +1,13 @@
+//
+//  YumemiWeatherAPIClient.swift
+//  iOS-Training-Ryuto
+//
+//  Created by 岩本 竜斗 on 2023/06/07.
+//
+
+import Foundation
+import Combine
+
+final class YumemiWeatherAPIClient {
+    
+}
