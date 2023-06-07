0724a2139b28ff9c21965319633e30b3b679cb5e
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-07 15:10:34 +0900

    feat: 天気画面のUIを実装した

--- iOS-Training-Ryuto/WeatherView.swift
+++ iOS-Training-Ryuto/WeatherView.swift
@@ -14,6 +14,59 @@ struct WeatherView: View {
                            startPoint: .top,
                            endPoint: .bottom)
             .ignoresSafeArea()
+            
+            VStack {
+                Text("東京")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.top, 20)
+                    .padding(.bottom, 10)
+                
+                Text("2023年6月7日の天気")
+                    .font(.title2)
+                
+                Spacer()
+                    .frame(height: 100)
+                
+                HStack {
+                    VStack(spacing: 30) {
+                        Image(systemName: "sun.max.fill")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 150, height: 150)
+                            .foregroundColor(.orange)
+                        
+                        Text("晴れ")
+                            .font(.system(size: 15))
+                    }
+                    
+                    Spacer()
+                        .frame(width: 40)
+                    
+                    VStack(spacing: 10) {
+                        HStack(spacing: 3) {
+                            Image(systemName: "arrow.up")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 20, height: 20)
+                            Text("最高: 30°")
+                        }
+                        .foregroundColor(.red)
+                        
+                        HStack(spacing: 3) {
+                            Image(systemName: "arrow.down")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 20, height: 20)
+                            Text("最高: 15°")
+                        }
+                        .foregroundColor(.indigo)
+                    }
+                    .font(.system(size: 20))
+                }
+                Spacer()
+            }
+            .foregroundColor(.white)
         }
     }
 }
