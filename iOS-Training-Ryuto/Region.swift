6295b07ea58dfab3bf3c930588695bc029b4c1c1
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-09 14:56:18 +0900

    add: Region.swiftを追加

---
+++ iOS-Training-Ryuto/Region.swift
@@ -0,0 +1,13 @@
+//
+//  Region.swift
+//  iOS-Training-Ryuto
+//
+//  Created by 岩本 竜斗 on 2023/06/09.
+//
+
+import Foundation
+
+struct Region {
+    let name: String
+    let prefectures: [Prefecture]
+}
