af281e395900e98db4cfdd8cede57b4adfeb6ac9
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-09 14:21:39 +0900

    delete: dateToISO8601String関数とそのプロパティを削除

--- iOS-Training-Ryuto/Helper/DateHelper.swift
+++ iOS-Training-Ryuto/Helper/DateHelper.swift
@@ -9,13 +9,9 @@ import Foundation
 
 final class DateHelper {
     private let dateFormatter: DateFormatter
-    private let iso8601DateFormatter: ISO8601DateFormatter
     static let shared = DateHelper()
     
     private init() {
-        self.iso8601DateFormatter = ISO8601DateFormatter()
-        self.iso8601DateFormatter.timeZone = TimeZone(identifier: "Asia/Tokyo")
-        
         self.dateFormatter          = DateFormatter()
         self.dateFormatter.calendar = Calendar(identifier: .gregorian)
         self.dateFormatter.locale   = Locale(identifier: "jp_JP")
@@ -26,8 +22,5 @@ extension DateHelper {
     func formatToString(date: Date, dateFormat: String) -> String {
         dateFormatter.dateFormat = dateFormat
         return dateFormatter.string(from: date)
-    }
-    func dateToISO8601String(date: Date) -> String {
-        return iso8601DateFormatter.string(from: date)
     }
 }
