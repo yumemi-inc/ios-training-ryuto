d3bcbc7135d3dfdc5537f97e398806d613f7d6d2
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-09 12:46:01 +0900

    feat: JsonHelperにencoder, decoder, sharedプロパティを追加

--- iOS-Training-Ryuto/Helper/JsonHelper.swift
+++ iOS-Training-Ryuto/Helper/JsonHelper.swift
@@ -8,6 +8,19 @@
 import Foundation
 
 final class JsonHelper {
+    private let encoder: JSONEncoder
+    private let decoder: JSONDecoder
+    
+    static let shared = JsonHelper()
+    
+    private init() {
+        self.encoder = JSONEncoder()
+        self.encoder.dateEncodingStrategy = .iso8601
+        
+        self.decoder = JSONDecoder()
+        self.decoder.dateDecodingStrategy = .iso8601
+    }
+    
     static func createJSON(_ object: [String: String]) throws -> String {
         do {
             let jsonData = try JSONSerialization.data(withJSONObject: object)
