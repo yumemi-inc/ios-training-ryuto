515ecd7b6c4aed8ae55515424b69955accbb3e28
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-09 10:21:10 +0900

    DateHelperにdateFormatterプロパティを追加

--- iOS-Training-Ryuto/DateHelper.swift
+++ iOS-Training-Ryuto/DateHelper.swift
@@ -8,12 +8,17 @@
 import Foundation
 
 final class DateHelper {
+    private let dateFormatter: DateFormatter
     private let iso8601DateFormatter: ISO8601DateFormatter
     static let shared = DateHelper()
     
     private init() {
         self.iso8601DateFormatter = ISO8601DateFormatter()
         self.iso8601DateFormatter.timeZone = TimeZone(identifier: "Asia/Tokyo")
+        
+        self.dateFormatter          = DateFormatter()
+        self.dateFormatter.calendar = Calendar(identifier: .gregorian)
+        self.dateFormatter.locale   = Locale(identifier: "jp_JP")
     }
 }
 
