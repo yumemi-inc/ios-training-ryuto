5a5215e77298fbb575a53f4d770196279da79792
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-09 14:55:17 +0900

    add: Prefecture.swiftを追加

---
+++ iOS-Training-Ryuto/Prefecture.swift
@@ -0,0 +1,13 @@
+//
+//  Prefecture.swift
+//  iOS-Training-Ryuto
+//
+//  Created by 岩本 竜斗 on 2023/06/09.
+//
+
+import Foundation
+
+struct Prefecture {
+    let name: String
+    let imageName: String
+}
