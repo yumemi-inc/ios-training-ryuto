90cdce4a98aad2868e823ae6376c239d640e2e62
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-09 10:36:48 +0900

    フォルダ階層を整理した

---
+++ iOS-Training-Ryuto/Helper/JsonHelper.swift
@@ -0,0 +1,31 @@
+//
+//  JsonHelper.swift
+//  iOS-Training-Ryuto
+//
+//  Created by 岩本 竜斗 on 2023/06/07.
+//
+
+import Foundation
+
+final class JSONHelper {
+    static func encodeToString<T: Encodable>(_ model: T) throws -> String? {
+        do {
+            let encoder = JSONEncoder()
+            encoder.dateEncodingStrategy = .iso8601
+            let jsonData = try encoder.encode(model)
+            return String(data: jsonData, encoding: .utf8)
+        } catch {
+            throw error
+        }
+    }
+    
+    static func decode<T: Decodable>(_ model: T.Type, data: Data) throws -> T {
+        do {
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            return try decoder.decode(model, from: data)
+        } catch {
+            throw error
+        }
+    }
+}
