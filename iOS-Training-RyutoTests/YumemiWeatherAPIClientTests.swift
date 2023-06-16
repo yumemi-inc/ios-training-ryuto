0e015f0ffae0e39743e00fe61bb5f960c1717019
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-16 15:41:34 +0900

    feat: fetchWeatherCondition関数をテストする関数を追加

--- iOS-Training-RyutoTests/YumemiWeatherAPIClientTests.swift
+++ iOS-Training-RyutoTests/YumemiWeatherAPIClientTests.swift
@@ -20,4 +20,70 @@ final class YumemiWeatherAPIClientTests: XCTestCase {
         client = YumemiWeatherAPIClient()
         subscriptions = Set<AnyCancellable>()
     }
+    
+    func testFetchWeatherCondition_ValidJSON() {
+        
+        let jsonString = """
+            {
+                "area": "tokyo",
+                "date": "2023-06-16T12:00:00+09:00"
+            }
+            """
+        let expectation = self.expectation(description: "非同期処理")
+        
+        client.weather
+            .sink(receiveValue: { [weak self] result in
+                switch result {
+                case .success(let weather):
+                    self?.weather = weather
+                case .failure(let error):
+                    self?.error = error
+                }
+                
+                expectation.fulfill()
+            })
+            .store(in: &subscriptions)
+        
+        client.fetchWeatherCondition(jsonString: jsonString)
+        
+        wait(for: [expectation], timeout: 10)
+        
+        if let weather = weather {
+            XCTAssertTrue(true)
+        } else {
+            XCTAssertNotNil(error)
+            XCTAssertEqual(error, .unknownError)
+        }
+    }
+    
+    func testFetchWeatherCondition_InvalidJSON() {
+        
+        let jsonString = """
+                {
+                    "area": tokyo,
+                }
+                """
+        let expectation = self.expectation(description: "非同期処理")
+        
+        client.weather
+            .sink(receiveValue: { [weak self] result in
+                switch result {
+                case .failure(let error):
+                    self?.error = error
+                default:
+                    break
+                }
+                
+                expectation.fulfill()
+            })
+            .store(in: &subscriptions)
+        
+        client.fetchWeatherCondition(jsonString: jsonString)
+        
+        wait(for: [expectation], timeout: 10)
+        
+        XCTAssertNotNil(error)
+        XCTAssertEqual(error, .invalidParameterError)
+        
+    }
 }
