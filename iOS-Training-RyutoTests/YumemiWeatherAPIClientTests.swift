ef1c0480251a3b302b4ef2456f69c8c5b42935a3
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-16 12:55:05 +0900

    add:　iOS-Training-RyutoTests/YumemiWeatherAPIClientTests.swiftを追加

---
+++ iOS-Training-RyutoTests/YumemiWeatherAPIClientTests.swift
@@ -0,0 +1,12 @@
+//
+//  YumemiWeatherAPIClientTests.swift
+//  iOS-Training-RyutoTests
+//
+//  Created by 岩本 竜斗 on 2023/06/16.
+//
+
+import XCTest
+
+final class YumemiWeatherAPIClientTests: XCTestCase {
+    
+}
