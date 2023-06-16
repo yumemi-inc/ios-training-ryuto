c41105f5de1867dbf14e3ddaa15e05e9766299a1
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-16 09:55:27 +0900

    delete: いらないプロパティを削除

--- iOS-Training-Ryuto/Helper/DateHelper.swift
+++ iOS-Training-Ryuto/Helper/DateHelper.swift
@@ -8,17 +8,6 @@
 import Foundation
 
 final class DateHelper {
-    private let dateFormatter: DateFormatter
-    static let shared = DateHelper()
-    
-    private init() {
-        self.dateFormatter          = DateFormatter()
-        self.dateFormatter.calendar = Calendar(identifier: .gregorian)
-        self.dateFormatter.locale   = Locale(identifier: "jp_JP")
-    }
-}
-
-extension DateHelper {
     static func formatToString(date: Date, dateFormat: String) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.calendar = Calendar(identifier: .gregorian)
