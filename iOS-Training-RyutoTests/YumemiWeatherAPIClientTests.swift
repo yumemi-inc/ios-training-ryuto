fd0576ba5792d838b16e96b59b92b443f2367cd7
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-16 15:01:32 +0900

    feat: テストに必要なプロパティを追加

--- iOS-Training-RyutoTests/YumemiWeatherAPIClientTests.swift
+++ iOS-Training-RyutoTests/YumemiWeatherAPIClientTests.swift
@@ -6,12 +6,18 @@
 //
 
 import XCTest
+import Combine
+import YumemiWeather
 @testable import iOS_Training_Ryuto
 
 final class YumemiWeatherAPIClientTests: XCTestCase {
     var client: YumemiWeatherAPIClient!
+    var subscriptions: Set<AnyCancellable>!
+    var weather: Weather?
+    var error: YumemiWeatherError?
     
     override func setUp() {
         client = YumemiWeatherAPIClient()
+        subscriptions = Set<AnyCancellable>()
     }
 }
