6a0bc18ef33389a3c79bc5635b33230f43935a05
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-20 18:36:58 +0900

    feat: testFetchWeatherCondition関数を追加

--- iOS-Training-RyutoTests/WeatherViewModelTest.swift
+++ iOS-Training-RyutoTests/WeatherViewModelTest.swift
@@ -6,8 +6,29 @@
 //
 
 import XCTest
+import YumemiWeather
 @testable import iOS_Training_Ryuto
 
 final class WeatherViewModelTest: XCTestCase {
     var viewModel: WeatherViewModel!
+    
+    func testFetchWeatherCondition() {
+        XCTContext.runActivity(named: "正常") { _ in
+            viewModel = WeatherViewModel(yumemiWeatherAPIClient: YumemiWeatherAPIClientMock())
+            viewModel.fetchWeatherCondition(area: "東京", date: Date())
+            
+            XCTAssertNil(viewModel.yumemiWeatherError)
+            XCTAssertEqual(viewModel.weather?.condition, YumemiWeatherSampleData.sampleData.condition)
+            XCTAssertEqual(viewModel.weather?.maxTemperature, YumemiWeatherSampleData.sampleData.maxTemperature)
+            XCTAssertEqual(viewModel.weather?.minTemperature, YumemiWeatherSampleData.sampleData.minTemperature)
+        }
+        
+        XCTContext.runActivity(named: "エラー") { _ in
+            viewModel = WeatherViewModel(yumemiWeatherAPIClient: YumemiWeatherAPIClientMock(yumemiWeatherError: .unknownError))
+            viewModel.fetchWeatherCondition(area: "東京", date: Date())
+            
+            XCTAssertNil(viewModel.weather)
+            XCTAssertEqual(viewModel.yumemiWeatherError, .unknownError)
+        }
+    }
 }
