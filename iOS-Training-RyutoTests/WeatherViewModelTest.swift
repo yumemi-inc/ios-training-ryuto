a2dec5ea9d8120dec20f138b47936c27031109a8
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-20 18:28:26 +0900

    add: WeatherViewModelTest.swiftを通過

---
+++ iOS-Training-RyutoTests/WeatherViewModelTest.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherViewModelTest.swift
+//  iOS-Training-RyutoTests
+//
+//  Created by 岩本 竜斗 on 2023/06/20.
+//
+
+import XCTest
+@testable import iOS_Training_Ryuto
+
+final class WeatherViewModelTest: XCTestCase {
+    var viewModel: WeatherViewModel!
+}
