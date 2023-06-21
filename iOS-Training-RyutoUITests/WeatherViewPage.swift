4c51e6aef107d9f8e74abd3016675b9657a0673d
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-21 15:48:47 +0900

    feat: UI要素のプロパティを追加

--- iOS-Training-RyutoUITests/WeatherViewPage.swift
+++ iOS-Training-RyutoUITests/WeatherViewPage.swift
@@ -12,16 +12,31 @@ final class WeatherViewPage: PageObject {
     
     enum IDs {
         static let prefectureText = "weather_prefecture_name"
-        static let conditionText = "weather_condition_image"
-        static let conditionImage = "weather_condition_text"
+        static let conditionImage = "weather_condition_image"
+        static let conditionText = "weather_condition_text"
     }
     
     private let app: XCUIApplication
     required init(application: XCUIApplication) {
         self.app = application
     }
+    
+    var prefectureText: XCUIElement {
+        return app.staticTexts[IDs.prefectureText]
+    }
+    
+    var conditionText: XCUIElement {
+        return app.staticTexts[IDs.conditionText]
+    }
+    
+    var conditionImage: XCUIElement {
+        return app.images[IDs.conditionImage]
+    }
 }
 
 extension WeatherViewPage {
     var existsPage: Bool { true }
+    var existsPrefectureText: Bool { prefectureText.exists }
+    var existsConditionText: Bool { conditionText.exists }
+    var existsConditionImage: Bool { conditionImage.exists }
 }
