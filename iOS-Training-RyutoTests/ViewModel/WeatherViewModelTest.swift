13a8634e9b18ec50733cdb33d18cdde71ebed0b8
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-23 17:41:37 +0900

    fix: WeatherViewModelTestを書き直した

--- iOS-Training-RyutoTests/ViewModel/WeatherViewModelTest.swift
+++ iOS-Training-RyutoTests/ViewModel/WeatherViewModelTest.swift
@@ -15,37 +15,69 @@ final class WeatherViewModelTest: XCTestCase {
     
     func testFetchWeatherCondition_Valid() {
         viewModel = WeatherViewModel(yumemiWeatherAPIClient: YumemiWeatherAPIClientMock(weather: YumemiWeatherSampleData.sampleData))
+        let expectation = XCTestExpectation(description: "非同期処理")
+        
         viewModel.fetchWeatherCondition(area: "東京", date: Date())
         
-        XCTAssertNil(viewModel.yumemiWeatherError)
-        XCTAssertEqual(viewModel.weather?.condition, YumemiWeatherSampleData.sampleData.condition)
-        XCTAssertEqual(viewModel.weather?.maxTemperature, YumemiWeatherSampleData.sampleData.maxTemperature)
-        XCTAssertEqual(viewModel.weather?.minTemperature, YumemiWeatherSampleData.sampleData.minTemperature)
+        Task { @MainActor in
+            XCTAssertNil(viewModel.yumemiWeatherError)
+            XCTAssertEqual(viewModel.weather?.condition, YumemiWeatherSampleData.sampleData.condition)
+            XCTAssertEqual(viewModel.weather?.maxTemperature, YumemiWeatherSampleData.sampleData.maxTemperature)
+            XCTAssertEqual(viewModel.weather?.minTemperature, YumemiWeatherSampleData.sampleData.minTemperature)
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 3.0)
     }
     
     func testFetchWeatherCondition_Error() {
         viewModel = WeatherViewModel(yumemiWeatherAPIClient: YumemiWeatherAPIClientMock(yumemiWeatherError: .unknownError))
+        let expectation = XCTestExpectation(description: "非同期処理")
+        
         viewModel.fetchWeatherCondition(area: "東京", date: Date())
         
-        XCTAssertNil(viewModel.weather)
-        XCTAssertEqual(viewModel.yumemiWeatherError, .unknownError)
+        Task { @MainActor in
+            XCTAssertNil(viewModel.weather)
+            XCTAssertEqual(viewModel.yumemiWeatherError, .unknownError)
+            
+            expectation.fulfill()
+        }
+        
+        wait(for: [expectation], timeout: 3.0)
     }
     
     func testAsyncFetchWeather_Valid() async {
         viewModel = WeatherViewModel(yumemiWeatherAPIClient: YumemiWeatherAPIClientMock(weather: YumemiWeatherSampleData.sampleData))
+        let expectation = XCTestExpectation(description: "非同期処理")
+        
         await viewModel.asyncFetchWeather(area: "東京", date: Date())
         
-        XCTAssertNil(viewModel.yumemiWeatherError)
-        XCTAssertEqual(viewModel.weather?.condition, YumemiWeatherSampleData.sampleData.condition)
-        XCTAssertEqual(viewModel.weather?.maxTemperature, YumemiWeatherSampleData.sampleData.maxTemperature)
-        XCTAssertEqual(viewModel.weather?.minTemperature, YumemiWeatherSampleData.sampleData.minTemperature)
+        Task { @MainActor in
+            XCTAssertNil(viewModel.yumemiWeatherError)
+            XCTAssertEqual(viewModel.weather?.condition, YumemiWeatherSampleData.sampleData.condition)
+            XCTAssertEqual(viewModel.weather?.maxTemperature, YumemiWeatherSampleData.sampleData.maxTemperature)
+            XCTAssertEqual(viewModel.weather?.minTemperature, YumemiWeatherSampleData.sampleData.minTemperature)
+            
+            expectation.fulfill()
+        }
+        
+        await fulfillment(of: [expectation], timeout: 5.0)
     }
     
     func testAsyncFetchWeather_Error() async {
         viewModel = WeatherViewModel(yumemiWeatherAPIClient: YumemiWeatherAPIClientMock(yumemiWeatherError: .unknownError))
+        let expectation = XCTestExpectation(description: "非同期処理")
+        
         await viewModel.asyncFetchWeather(area: "東京", date: Date())
         
-        XCTAssertNil(viewModel.weather)
-        XCTAssertEqual(viewModel.yumemiWeatherError, .unknownError)
+        Task { @MainActor in
+            XCTAssertNil(viewModel.weather)
+            XCTAssertEqual(viewModel.yumemiWeatherError, .unknownError)
+            
+            expectation.fulfill()
+        }
+        
+        await fulfillment(of: [expectation], timeout: 5.0)
     }
 }
