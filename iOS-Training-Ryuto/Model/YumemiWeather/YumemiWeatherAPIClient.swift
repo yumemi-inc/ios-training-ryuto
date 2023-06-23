4f14823463664bdbd1e24f4c065b3645d0c8eeb3
Yuzuki0709 <ryuto2001@ezweb.ne.jp>
2023-06-23 12:42:53 +0900

    feat: YumemiWeatherAPIClientProtocolにasyncFetchWeather関数を追加

--- iOS-Training-Ryuto/Model/YumemiWeather/YumemiWeatherAPIClient.swift
+++ iOS-Training-Ryuto/Model/YumemiWeather/YumemiWeatherAPIClient.swift
@@ -11,6 +11,7 @@ import YumemiWeather
 
 protocol YumemiWeatherAPIClientProtocol {
     func fetchWeatherCondition(jsonString: String) throws -> Weather?
+    func asyncFetchWeather(jsonString: String) async throws -> Weather?
 }
 
 final class YumemiWeatherAPIClient: YumemiWeatherAPIClientProtocol {
@@ -19,4 +20,36 @@ final class YumemiWeatherAPIClient: YumemiWeatherAPIClientProtocol {
         guard let responseData = response.data(using: .utf8) else { return nil }
         return try JSONHelper.decode(Weather.self, data: responseData)
     }
+    
+    func asyncFetchWeather(jsonString: String) async throws -> Weather? {
+        let response = try await YumemiWeather.asyncFetchWeather(jsonString)
+        guard let responseData = response.data(using: .utf8) else { return nil }
+        return try JSONHelper.decode(Weather.self, data: responseData)
+    }
 }
+
+#if DEBUG
+final class YumemiWeatherAPIClientMock: YumemiWeatherAPIClientProtocol {
+    let weather: Weather?
+    let yumemiWeatherError: YumemiWeatherError?
+    
+    init(weather: Weather? = nil, yumemiWeatherError: YumemiWeatherError? = nil) {
+        self.weather = weather
+        self.yumemiWeatherError = yumemiWeatherError
+    }
+    
+    func fetchWeatherCondition(jsonString: String) throws -> Weather? {
+        if let yumemiWeatherError = yumemiWeatherError {
+            throw yumemiWeatherError
+        }
+        return weather
+    }
+    
+    func asyncFetchWeather(jsonString: String) async throws -> Weather? {
+        if let yumemiWeatherError = yumemiWeatherError {
+            throw yumemiWeatherError
+        }
+        return weather
+    }
+}
+#endif
